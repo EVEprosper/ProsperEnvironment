9a00b1da85a5d0d2e00df691fdfcce7dac869760
Lockefox <locke.renard@gmail.com>
2016-10-05 16:19:52 -0700

    adding virtualenv wrapper for prosper projects

---
+++ prosper_virtualenv/setup.py
@@ -0,0 +1,47 @@
+'''wheel setup for Prosper development environment'''
+
+from os import path, listdir
+from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+
+def include_all_subfiles(path_included):
+    '''for data_files {path_included}/*'''
+    local_path = path.join(HERE, path_included)
+    file_list = []
+
+    for file in listdir(local_path):
+        file_list.append(path_included + '/' + file)
+
+    return file_list
+
+def hack_find_packages(include_str):
+    '''setuptools.find_packages({include_str}) does not work.  Adjust pathing'''
+    new_list = [include_str]
+    for element in find_packages(include_str):
+        new_list.append(include_str + '.' + element)
+
+    return new_list
+
+setup(
+    name='prosper_virtualenv',
+    version='0.0.0',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords='prosper eveonline api environment',
+    packages=find_packages(),
+    data_files={
+        ('docs', include_all_subfiles('docs')),
+    },
+    package_data={
+
+    },
+    install_requires=[
+
+    ],
+    dependency_links=[
+
+    ]
+)
